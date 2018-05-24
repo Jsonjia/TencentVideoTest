decf11bee6940d1bc89cb9457f6e65bd6c8af0be
jsonzjp <jiazjp@163.com>
2018-05-24 17:32:48 +0800

    添加版本测试

--- app/src/main/java/com/zjp/tencentvideo/MainActivity.java
+++ app/src/main/java/com/zjp/tencentvideo/MainActivity.java
@@ -2,6 +2,9 @@ package com.zjp.tencentvideo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.tencent.rtmp.TXLiveBase;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String versionStr = TXLiveBase.getSDKVersionStr();
+        Log.d("zjp", "versinoStr=" + versionStr);
     }
 }
